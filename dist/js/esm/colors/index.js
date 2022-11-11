72212584bd3a3cc79657207ccb5d9deca1e0b817
Matt Oliver <mattoliver.mattoliver@gmail.com>
2022-11-11 03:36:42 -0600

    rm cached dist from git and update .gitignore

--- dist/js/esm/colors/index.js
+++
@@ -1,5 +0,0 @@
-import {light} from './light'
-import {dark} from './dark'
-import {black} from './black'
-import {hacker} from './hacker'
-export default { light, dark, black, hacker }
--- dist/js/index.js
+++
@@ -1,9 +0,0 @@
-export default {
-  "width": "1px",
-  "style": "solid",
-  "radius": {
-    "small": "4px",
-    "medium": "8px",
-    "big": "16px"
-  }
-}
